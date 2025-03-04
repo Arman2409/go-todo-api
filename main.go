bd4fbb682f3af83ae2d3a401daa38490b57d39f0
Arman <armanghazaryan2409@gmail.com>
2025-03-04 19:39:02 +0400

    init: setup project

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+
+	"todo/handlers"
+	"todo/prisma/db"
+)
+
+func main() {
+    router := gin.Default()
+
+    prismaClient := db.NewClient();
+
+    err := prismaClient.Prisma.Connect()
+    if err != nil {
+        log.Fatalf("Failed to connect to the database: %v", err)
+    }
+    
+    defer prismaClient.Prisma.Disconnect() 
+    
+    todoHandler := handlers.TodoHandler{Prisma: prismaClient}
+
+    router.GET("/todos", todoHandler.GetTodos)
+    router.POST("/todo", todoHandler.CreateTodo)
+    router.PATCH("/todo/:id", todoHandler.UpdateTodo)
+
+    router.Run(":8080")
+}
